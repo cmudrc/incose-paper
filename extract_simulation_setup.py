c81abaab2509fdc65be4a6d047e789ec220d42cb
Aslan Noorghasemi <anoorghasemi@gmail.com>
2025-05-02 14:17:44 -0400

    Relocating simulation, including result and analysis to this repo.

---
+++ extract_simulation_setup.py
@@ -0,0 +1,27 @@
+import os
+import csv
+
+import piperabm as pa
+
+from info import *
+
+
+path = os.path.dirname(os.path.realpath(__file__))
+filepath = os.path.join(path, 'result')
+
+# Save model setups to file
+headers = ['name', 'population', 'household_size', 'seed', 'impact_type', 'impact_level']
+filename = os.path.join(filepath, 'setups.csv')
+with open(filename, mode='w', newline='') as file:
+    writer = csv.writer(file)
+    writer.writerow(headers)
+for name in names:
+    setup = name_to_setup(name)
+    if setup['impact'] is None:
+        impact_info = ['none', 'none']
+    else:
+        impact_info = setup['impact']
+    row = [name, setup['population'], setup['household_size'], setup['seed'], impact_info[0], impact_info[1]]
+    with open(filename, mode='a', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerow(row)
