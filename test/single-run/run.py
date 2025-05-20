c587d858206647569d17f9268c82b1cd64a1a647
Aslan Noorghasemi <anoorghasemi@gmail.com>
2025-05-20 09:17:38 -0400

    run in progress

--- test/single-run/run.py
+++ test/single-run/run.py
@@ -5,17 +5,28 @@ import os
 
 import piperabm as pa
 
-
+# Setup model
 path = os.path.dirname(os.path.realpath(__file__))
 model = pa.Model(path=path, seed=2)
 
+# Setup infrastructure
 model.infrastructure.generate(
     homes_num=5,
     grid_size=[10, 10],
     grid_num=[5, 5]
 )
+
+# Setup society
 model.society.generate(
     num=10,
     gini_index=0.4
 )
+
+# Run the model
 model.run(n=100, step_size=7200, save=True, save_transactions=True, report=True)
+
+# Measure
+measurement = pa.Measurement(path=path)
+measurement.measure(report=True, resume=False)
+measurement.accessibility.save()
+measurement.travel_distance.save()
